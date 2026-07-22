95da167871cbea07e08b0bf838ea6ce398160b60
Ankit10M <ankitbrijeshmishra10@gmail.com>
2026-07-22 17:35:40 +0530

    feat: implement controller methods for listing, clearing, and deleting user search history

--- src/modules/user/searchHistory.controller.js
+++ src/modules/user/searchHistory.controller.js
@@ -1,4 +1,4 @@
-const SearchHistory = require('./searchHistory.model');
+6const SearchHistory = require('./searchHistory.model');
 const ApiError = require('../../utils/ApiError');
 const ApiResponse = require('../../utils/ApiResponse');
 const asyncHandler = require('../../utils/asyncHandler');
